c40a6a01d27861ea52eacdf5c1e68b4fd0d45ce8
Aija Koivistoinen <aija.koivistoinen@edu.savonia.fi>
2023-02-02 01:16:27 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import kuva3 from './kuva3.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="header">
+  <h1>How to mask old skin</h1>
+</div>
+
+<div class="row">
+  <div class="column side">
+    <img className='kuva' src={kuva3}></img>
+  </div>
+  
+  <div class="column middle">
+    <h3>To disguise older skin and address its issues, consider these steps:</h3>
+    <ol className='lista'>
+      <li>Cleanse the skin to prepare for masking.</li>
+      <li>Use a light exfoliant to remove dead skin cells and smooth the skin's surface.</li>
+      <li>Apply a concealer or color corrector to target specific aging issues, such as dark spots or fine</li>
+      <li>Blend well to ensure a natural look.</li>
+      <li>Finish with a light layer of foundation or powder.</li>
+    </ol>
+    <h3>Problems with disguising older skin include:</h3>
+    <ol className='lista'>
+      <li>Poor application technique can make the masking look unnatural.</li>
+      <li>Choosing the wrong product or color can draw attention to aging issues rather than disguise them.</li>
+      <li>Overuse of heavy makeup can accentuate fine lines and wrinkles.</li>
+      <li>Not taking care of the skin underneath the makeup can lead to more aging issues.</li>
+      <li>Neglecting to cleanse and moisturize properly can lead to skin irritation and breakouts.</li>
+    </ol>
+
+  </div>
+  <div class="column side">
+    <img className='kuva' src={kuva3}></img>
+  </div>
+</div>
+  <footer class="footer">
+    <p>Keywords: older, masking, issues</p>
+  </footer>
     </div>
   );
 }
